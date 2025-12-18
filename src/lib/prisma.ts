dab9233188ad18484fada681e130f0902bb7c96a
sknaveed38 <sknaveedsk20@gmail.com>
2025-12-18 16:30:06 +0530

    c8

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,10 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
-// import Database from 'better-sqlite3' // Not needed anymore
+import { PrismaClient } from '@prisma/client';
+import { PrismaLibSQL } from '@prisma/adapter-libsql';
 
-const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL! }); // Pass the config object
-const prisma = new PrismaClient({ adapter });
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export default prisma
+const adapter = new PrismaLibSQL({ url: process.env.DATABASE_URL! });
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
